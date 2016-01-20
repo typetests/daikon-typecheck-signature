0cfcfcfd637a4371a673421a99919ccc41823000
Michael Ernst <mernst@cs.washington.edu>
2016-01-20 11:35:33 -0800

    Initial revision

---
+++ .travis-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ROOT=$TRAVIS_BUILD_DIR/..
+
+# Fail the whole script if any command fails
+set -e
+
+## Build Checker Framework
+(cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
+# This also builds annotation-tools and jsr308-langtools
+(cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh)
+export CHECKERFRAMEWORK=$ROOT/checker-framework
+
+## Obtain daikon
+(cd $ROOT && git clone https://github.com/codespecs/daikon.git)
+
+make -C $ROOT/daikon/java check-signature
