2158c8d27174c51d793174ee6e0bab1a9195c34e
Michael Ernst <mernst@cs.washington.edu>
2016-05-08 08:45:28 -0700

    Reduce git clone depth

--- .travis-build.sh
+++ .travis-build.sh
@@ -5,13 +5,13 @@ ROOT=$TRAVIS_BUILD_DIR/..
 set -e
 
 ## Build Checker Framework
-(cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
+(cd $ROOT && git clone --depth 1 https://github.com/typetools/checker-framework.git)
 # This also builds annotation-tools and jsr308-langtools
 (cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh)
 export CHECKERFRAMEWORK=$ROOT/checker-framework
 
 ## Obtain daikon
-(cd $ROOT && git clone https://github.com/codespecs/daikon.git)
+(cd $ROOT && git clone --depth 1 https://github.com/codespecs/daikon.git)
 make -C $ROOT/daikon/java compile dyncomp-jdk
 make -C $ROOT/daikon daikon.jar
 
