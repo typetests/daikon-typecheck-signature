6ac48283e313550186516c0e192e60099d3f536a
Michael Ernst <mernst@cs.washington.edu>
2017-08-13 07:46:41 -0700

    Download the JDK rather than rebuilding it

--- .travis-build.sh
+++ .travis-build.sh
@@ -7,7 +7,7 @@ set -e
 ## Build Checker Framework
 (cd $ROOT && git clone --depth 1 https://github.com/typetools/checker-framework.git)
 # This also builds annotation-tools and jsr308-langtools
-(cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh)
+(cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh downloadjdk)
 export CHECKERFRAMEWORK=$ROOT/checker-framework
 
 ## Obtain daikon
